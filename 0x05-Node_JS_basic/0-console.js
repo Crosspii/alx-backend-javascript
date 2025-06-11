6ccbb50705febabcf5a04c504aec65bf1fbb3232
Aymen hommani <crosspieloo@gmail.com>
2025-06-11 05:35:34 +0100

    uploading the finished js node modules after deleting the node testing directory

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message to the stdout.
+ * @param {string} message - The message to display.
+ * @returns {void}
+ */
+// Arrow function to display a message
+const displayMessage = (message) => console.log(message);
+
+module.exports = displayMessage;
